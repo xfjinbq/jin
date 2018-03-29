53e0351de61acc7d7820e3e2b909ba7cbeb63d44
xfjinbq <37624463+xfjinbq@users.noreply.github.com>
2018-03-29 11:20:27 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,7 +1,7 @@
 <?php
 define('ROOT_PART', Root_part());
 define('APIKEY', 'AIzaSyAtBTsaG6E7V_AOVOhLxqHh3PwrVrVU4ZM');
-define('GJ_CODE', 'KR');
+define('GJ_CODE', 'US');
 define('SITE_NAME', 'Youtube');
 define('TITLENAME', 'Youtube');
 define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh4');
